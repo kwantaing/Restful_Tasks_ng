17d64922a9769364dace4b2194780604748f20f1
Quentin Ng <qng1997@gmail.com>
2019-07-12 12:16:17 -0700

    PokeAPI assignment

--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -8,6 +8,8 @@ export class HttpService {
   constructor(private _http: HttpClient) { 
     this.getTasks();
     this.getTaskbyId();
+    this.getPokemon();
+    this.getOvergrow();
   }
 
   getTasks(){
@@ -17,5 +19,13 @@ export class HttpService {
   getTaskbyId(){
     let tempObservable = this._http.get('/tasks/5d28d4d2df39f15989bba5e8');  //tasks/:id
     tempObservable.subscribe(data => console.log("individual Task :",data ));
+  }
+  getPokemon(){
+    let bulbasaur = this._http.get("https://pokeapi.co/api/v2/pokemon/1");
+    bulbasaur.subscribe(data => console.log("Pokemon: ",data));
+  }
+  getOvergrow(){
+    let numPoke = this._http.get("https://pokeapi.co/api/v2/ability/65");
+    numPoke.subscribe(data =>console.log(data.pokemon.length+" pokemon have Overgrow!"));
   }
 }
